be34949b5f373e3ccc2e7a29f1f99ad29bf1e9c4
Shoukatrai <raishoukat033@gmail.com>
2025-03-09 12:29:41 +0500

    class 07 connecting monogDB

---
+++ class07/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = 2020;
+
+const mongoURI = "mongodb+srv://shoukatrai:shoukat123@cluster0.tde8j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+
+mongoose.connect(mongoURI)
+.then(()=>{
+    console.log("MongoDB connected")
+})
+.catch((error)=>{
+    console.log("MongoDB not connected", error.message)
+})
+
+// app.get("/", (req, res)=>{
+//     console.log(req.url)
+//     res.send("Hello server from")
+// })
+
+
+app.listen(PORT, ()=>console.log("Server running"))
